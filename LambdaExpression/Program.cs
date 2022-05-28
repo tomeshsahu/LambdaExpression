0b99b83b83a05bd7d4dae63f0a4b40edb9bc4004
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-05-29 00:34:53 +0530

    [TOMESHWAR]ADD:-ADD DEFALULT DATA

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LambdaExpression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-------Welcome To Person Data Management Program------");
+        lambdaExp management = new lambdaExp();
+        management.AddDefaultData();
+        Console.ReadLine();
+    }
+}
---
+++ LambdaExpression/lambdaExp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{ 
+    public class lambdaExp
+    {
+        List<Person> personList = new List<Person>();
+        public void AddDefaultData()
+        {
+            personList.Add(new Person { SSN = 1, Name = "Tomesh", Address = "Bhilai", Age = 28 });
+            personList.Add(new Person { SSN = 2, Name = "Ramesh", Address = "Mumbai", Age = 31 });
+            personList.Add(new Person { SSN = 3, Name = "Suresh", Address = "Goa", Age = 70 });
+            personList.Add(new Person { SSN = 4, Name = "Arun", Address = "Dhule", Age = 70 });
+            personList.Add(new Person { SSN = 5, Name = "Girish", Address = "Shirpur", Age = 24 });
+            personList.Add(new Person { SSN = 6, Name = "Rohit", Address = "Dhule", Age = 26 });
+            personList.Add(new Person { SSN = 7, Name = "Manda", Address = "Warud", Age = 65 });
+            personList.Add(new Person { SSN = 8, Name = "Harshit", Address = "Mumbai", Age = 16 });
+            Display(personList);
+        }
+        public void Display(IEnumerable<Person> list)
+        {
+            foreach (var person in list)
+            {
+                Console.WriteLine(person.SSN + " " + person.Name + " " + person.Address + " " + person.Age);
+            }
+        }
+
+    }
+}
